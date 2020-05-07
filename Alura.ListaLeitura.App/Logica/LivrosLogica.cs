650f07ea73af48fa1cada9a575034c5bb2470e84
Claudio <claudiomeirabs@gmail.comm>
2020-05-07 15:08:53 -0300

    Carrega a em ista os livros para ler

---
+++ Alura.ListaLeitura.App/Logica/LivrosLogica.cs
@@ -0,0 +1,31 @@
+using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public static class LivrosLogica
+    {
+
+        public static string CarregaLista(IEnumerable<Livro> livros)
+        {
+            var conteudoArquivo = HtmlUtils.CarregaArquivoHTML("lista");
+            foreach (var livro in livros)
+            {
+                conteudoArquivo = conteudoArquivo
+                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            }
+            return conteudoArquivo.Replace("#NOVO-ITEM#", "");
+        }
+
+        
+    }
+}
